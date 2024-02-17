988b29683851a4904b6afc061386a5a7743b9548
Gaddy <gatira661@gmail.com>
2024-02-17 14:55:06 +0700

    Added looping println saying "WOW"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 // When to commit?
+// git add / git checkout -b then push
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        for (int i=0;i<3;i++){
+            System.out.println("WOW");
+        }
     }
 }
